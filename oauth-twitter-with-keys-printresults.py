64e9011fa20ba94bd0b3ad83804600d10d684a0a
Anndo Ko <handcuffanndo@gmail.com>
2018-02-06 14:15:24 -0500

    create new files for the Tue lec

---
+++ oauth-twitter-with-keys-printresults.py
@@ -0,0 +1,26 @@
+from requests_oauthlib import OAuth1Session
+import secrets2
+import json
+
+client_key = secrets2.client_key
+client_secret = secrets2.client_secret
+
+resource_owner_key = secrets2.access_token
+resource_owner_secret = secrets2.access_token_secret
+
+protected_url = 'https://api.twitter.com/1.1/account/settings.json'
+
+oauth = OAuth1Session(client_key,
+                          client_secret=client_secret,
+                          resource_owner_key=resource_owner_key,
+                          resource_owner_secret=resource_owner_secret)
+
+protected_url = 'https://api.twitter.com/1.1/search/tweets.json'
+params = {'q':'food'}
+r = oauth.get(protected_url, params = params)
+r_dic = json.loads(r.text)
+
+for r in r_dic["statuses"]:
+    print(r["user"]["name"])
+    print(r["text"])
+    print("----------")
