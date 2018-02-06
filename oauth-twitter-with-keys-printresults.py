dfe2a4d518cea88b44def2dd35c68bcc684415a3
Anndo Ko <handcuffanndo@gmail.com>
2018-02-06 14:19:31 -0500

    fix details

--- oauth-twitter-with-keys-printresults.py
+++ oauth-twitter-with-keys-printresults.py
@@ -8,15 +8,15 @@ client_secret = secrets2.client_secret
 resource_owner_key = secrets2.access_token
 resource_owner_secret = secrets2.access_token_secret
 
-protected_url = 'https://api.twitter.com/1.1/account/settings.json'
+protected_url = "https://api.twitter.com/1.1/account/settings.json"
 
 oauth = OAuth1Session(client_key,
                           client_secret=client_secret,
                           resource_owner_key=resource_owner_key,
                           resource_owner_secret=resource_owner_secret)
 
-protected_url = 'https://api.twitter.com/1.1/search/tweets.json'
-params = {'q':'food'}
+protected_url = "https://api.twitter.com/1.1/search/tweets.json"
+params = {"q" : "food"}
 r = oauth.get(protected_url, params = params)
 r_dic = json.loads(r.text)
 
